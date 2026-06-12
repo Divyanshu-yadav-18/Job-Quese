ddb991caa280a4fb3a909c390bcd25fed033f006
rishabhyadav@gmail.com <rishabhyadav14065@gmail.com>
2026-06-12 13:58:29 +0530

    feat:add queue functionalities( pop, peek, wait, len)

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -1,8 +1,10 @@
 package queue
 
 import (
+	"context"
 	"errors"
 	"sync"
+	"time"
 
 	"github.com/Divyanshu-yadav-18/Job-Quese/internal/model"
 )
@@ -48,19 +50,64 @@ func (q *Queue) Push(task *model.Task) error {
 
 			q.tasks = append(q.tasks, nil)
 			copy(q.tasks[i+1:], q.tasks[i:])
-			q.tasks[i] = inserted
+			q.tasks[i] = task
 			inserted = true
 			break
-		} 
+		}
 	}
 
-	if !inserted{
+	if !inserted {
 		q.tasks = append(q.tasks, task)
 	}
 
 	select {
 	case q.notify <- struct{}{}:
-		default:
+	default:
 	}
 	return nil
+}
+
+func (q *Queue) Pop() (*model.Task, error) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	now := time.Now()
+	for i, task := range q.tasks{
+		if task.RunAt.After(now){
+			continue
+		}
+
+		q.tasks = append(q.tasks[:i], q.tasks[i+1:]...)
+
+		return task, nil
+	}
+
+	return nil, ErrQueueEmpty
+}
+
+func (q* Queue) Wait(ctx context.Context){
+	select{
+	case <- q.notify:
+	case <- ctx.Done():
+		case <- time.After(500 * time.Millisecond):
+	}
+}
+
+func (q *Queue) Len() int {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	return len(q.tasks)
+}
+
+func (q *Queue) Peek(n int) []*model.Task {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	if n > len(q.tasks){
+		n = len(q.tasks)
+	}
+
+	result := make([]*model.Task, n)
+	copy(result,q.tasks[:n])
+	return result
 }
